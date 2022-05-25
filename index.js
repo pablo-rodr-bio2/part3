4d1ddfe366ec40ea31f1a440f4ce3f6b3bd76721
pablo-rodr-bio2 <pablo.rodriguez.bio2@gmail.com>
2022-05-25 22:09:45 +0200

    Phonebook backend step3

--- index.js
+++ index.js
@@ -38,6 +38,17 @@ app.get("/api/persons", (request, response) => {
   response.json(persons)
 })
 
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find( person => person.id === id )
+  if(person) {
+    console.log("Request person: ", person)
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running in port ${PORT}`)
