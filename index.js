1db06f50d65e809830189a1b48f774607618ec5a
pablo-rodr-bio2 <pablo.rodriguez.bio2@gmail.com>
2022-05-25 22:35:11 +0200

    Phonebook backend step4

--- index.js
+++ index.js
@@ -49,6 +49,13 @@ app.get("/api/persons/:id", (request, response) => {
   }
 })
 
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
+  console.log("Deleting ", id)
+  response.status(204).end()
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running in port ${PORT}`)
