4ec8be7f9fc80a20e0ad4240a3d5b4bb069d6720
pablo-rodr-bio2 <pablo.rodriguez.bio2@gmail.com>
2022-05-26 22:19:52 +0200

    Phonebook backend step7 & step8

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
 
 let persons = [
   { 
@@ -25,6 +26,9 @@ let persons = [
 ]
 
 app.use(express.json())
+
+morgan.token('body', (req) => JSON.stringify(req.body));
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
 app.get("/info", (request, response) => {
   const today = new Date()
