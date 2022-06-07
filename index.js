45d1a5b69bc17c04b055adc7aec33ac7b58160bc
pablo-rodr-bio2 <pablo.rodriguez.bio2@gmail.com>
2022-06-07 18:12:01 +0200

    3.13: Phonebook database, step1

--- index.js
+++ index.js
@@ -1,31 +1,11 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
+const Person = require('./models/person')
 const morgan = require('morgan')
 const cors = require('cors')
 
-
-let persons = [
-  { 
-    "id": 1,
-    "name": "Arto Hellas", 
-    "number": "040-123456"
-  },
-  { 
-    "id": 2,
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
-  },
-  { 
-    "id": 3,
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
-  },
-  { 
-    "id": 4,
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
-  }
-]
+const mongoose = require('mongoose')
 
 app.use(express.json())
 app.use(cors())
@@ -45,7 +25,9 @@ app.get("/info", (request, response) => {
 })
 
 app.get("/api/persons", (request, response) => {
-  response.json(persons)
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
 })
 
 app.get("/api/persons/:id", (request, response) => {
