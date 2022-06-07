b75060356bcdec287c9e95567425c26003685772
pablo-rodr-bio2 <pablo.rodriguez.bio2@gmail.com>
2022-06-07 18:33:36 +0200

    3.14: Phonebook database, step2

--- index.js
+++ index.js
@@ -83,7 +83,7 @@ app.post("/api/persons", (request, response) => {
     )
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running in port ${PORT}`)
 })
